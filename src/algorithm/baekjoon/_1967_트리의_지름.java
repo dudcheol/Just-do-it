544cf23c6c532c4899ceaf5d697fdadb2d9a9134
dudcheol <95_pyc@naver.com>
2021-07-29 09:09:00 +0900

    백준 / 골드4 / 트리의 지름 - 풀이참고

--- src/algorithm/baekjoon/_1967_트리의_지름.java
+++ src/algorithm/baekjoon/_1967_트리의_지름.java
@@ -5,65 +5,53 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class _1967_트리의_지름 {
 
-	private static int N;
-	private static Node[] nodes;
+	private static int N,fnode,max;
 	private static boolean[] visited;
-	private static int[] memo;
+	private static List<int[]>[] nodes;
 	
-	private static class Node{
-		List<int[]> list;
-		int w;
-		
-		Node(){
-			list = new ArrayList<int[]>();
-		}
-	}
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N=Integer.parseInt(st.nextToken()); // 노드 수
-		nodes = new Node[N+1];
-		visited = new boolean[N+1];
+		nodes = new List[N+1];
+		for (int i = 1; i <= N; i++) {
+			nodes[i]=new ArrayList<>();
+		}
 		for (int i = 0; i < N-1; i++) {
 			st=new StringTokenizer(br.readLine());
 			int p = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
-			if(nodes[p]==null)nodes[p]=new Node();
-			nodes[p].list.add(new int[] {c,w});
+			nodes[p].add(new int[] {c,w});
+			nodes[c].add(new int[] {p,w});
 		}
 		
-		// 각 노드별 리프노드까지 갔을 때 최대 길이 구하기
-		memo = new int[N+1];
-		dfs(1);
+		// 임의의 노드에서 시작해서 가장 먼 노드 찾기
+		visited = new boolean[N+1];
+		dfs(1, 0);
 		
-		int max=0;
-		for (int i = 1; i <= N; i++) {
-			max=Math.max(max, memo[i]);
-		}
+		// 가장 먼 노드에서 시작해서 가장 먼 노드까지의 길이 구하기 -> 트리의 지름이 됨
+		Arrays.fill(visited, false);
+		dfs(fnode, 0);
+		
 		System.out.println(max);
 	}
 
-	private static int dfs(int i) {
-		visited[i]=true;
-		Node node = nodes[i];
-		int max=0;
-		int sum=0;
-		if(node==null)return max;
-		for(int[] e : node.list) {
-			if(visited[e[0]]) continue;
-			int res = dfs(e[0])+e[1];
-			max = Math.max(res, max);
-			sum+=res;
+	private static void dfs(int i, int len) {
+		if(max < len) {
+			max = len;
+			fnode = i;
 		}
-		memo[i]=sum;
-		return max;
+		visited[i]=true;
+		List<int[]> node = nodes[i];
+		if(node==null)return;
+		for(int[] e : node) {
+			if(visited[e[0]]) continue;
+			dfs(e[0], len+e[1]);
+		}
 	}
-
 }
